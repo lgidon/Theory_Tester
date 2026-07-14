23d3a1f569e602cdf73991264ff8b6db771237d5
lgidon <lgidon@gmail.com>
2026-07-14 10:15:22 +0000

    Migrated to alternative sqlite engine, as the previous one was not working with CGO_ENABLED=0.

--- db.go
+++ db.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"log"
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 type DBClient struct {
@@ -21,7 +21,7 @@ func InitDB(dbPath string) (*DBClient, error) {
 		return nil, fmt.Errorf("failed to create data directory: %w", err)
 	}
 
-	db, err := sql.Open("sqlite3", dbPath)
+	db, err := sql.Open("sqlite", dbPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open sqlite database: %w", err)
 	}
